6227b4c00e47bead1ec35521fda712700853d371
conert1 <conertmavuso@gmail.com>
2023-08-23 18:49:37 +0200

    api

---
+++ routes/models/subscriber.js
@@ -0,0 +1,24 @@
+//model used to interact with the database
+const mongoose = require('mongoose')
+// const { subscribe } = require('../subscribers')
+
+const subscriberSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    
+    subscribedToChannel: {
+        type: String,
+        required: true
+    },
+
+    subscribeDate: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+
+})
+
+module.exports = mongoose.model('Subscriber', subscriberSchema)
---
+++ routes/subscribers.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const router = express.Router()
+const Subscriber = require('./models/subscriber')
+
+//getting all
+router.get('/', async (req, res) =>{
+    try{
+        const subscribers = await Subscriber.find()
+        res.json(subscribers)
+    } catch (err){
+        res.status(500).json({ message: err.message})
+    }
+})
+// getting one
+router.get('/:id', (req, res) =>{
+    res.send(req.params.id)
+})
+// creating none
+router.post('/', async (req, res) =>{
+    const subscriber =  new Subscriber({
+        name: req.body.name,
+        subscribedToChannel: req.body.subscribedToChannel
+    })
+    try{
+        const newSubscriber = await subscriber.save()
+        res.status(201).json(newSubscriber)
+    }   catch(err){
+            res.status(400).json({message: err.message})
+        }
+})
+// updating one
+router.patch('/', (req, res)=>{
+
+})
+//deleting one
+router.delete('/:id', (req, res) =>{
+
+})
+
+
+module.exports = router
